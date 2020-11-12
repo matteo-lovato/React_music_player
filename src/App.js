f2b5fa1d9e1b76dbba9dd28b2316e76c67d7e8e0
matteo_lovato <lovato.teo@gmail.com>
2020-11-12 19:14:49 +0100

    added some songs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import "./styles/app.scss";
+import data from "./data";
 
 function App() {
+  // state
+  const [songs, setSongs] = useState(data());
   return (
     <div className="App">
       <Song />
---
+++ src/data.js
@@ -0,0 +1,209 @@
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+
+function data() {
+  return [
+    {
+      name: "Hidden Structure",
+      artist: "Leavv, Flitz&Suppe",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#6A9D92", "#F9F9F9"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9913",
+    },
+    {
+      name: "Dancing Droplets",
+      artist: "Leavv",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#6A9D92", "#F9F9F9"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9915",
+    },
+    {
+      name: "Flushing the Stairs",
+      artist: "Leavv",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/6defa6c89a6902fdd84bccd720c2b8fd29c40990-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#033742", "#929C70"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9917",
+    },
+    {
+      name: "Paved Paths",
+      artist: "Leavv",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#6A9D92", "#F9F9F9"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9919",
+    },
+    {
+      name: "Aqueduct",
+      artist: "Leavv",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#6A9D92", "#F9F9F9"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9921",
+    },
+    {
+      name: "What Was Before",
+      artist: "Philanthrope, Leavv",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#6A9D92", "#F9F9F9"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9923",
+    },
+    {
+      name: "With Time",
+      artist: "Leavv, The Field Tapes",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#6A9D92", "#F9F9F9"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9925",
+    },
+    {
+      name: "Zenith",
+      artist: "Leavv",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/88e7eb711f8c71d87fc102e97cf91e36f692348d-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#6A9D92", "#F9F9F9"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9927",
+    },
+    {
+      name: "Maple Leaf Pt.2",
+      artist: "Philanthrope",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A15D50", "#995A48"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10243",
+    },
+    {
+      name: "Wildlife",
+      artist: "Philanthrope, chromonicci",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A15D50", "#995A48"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10263",
+    },
+    {
+      name: "Upstate",
+      artist: "Philanthrope, Brock Berrigan, The Field Tapes",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A15D50", "#995A48"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10245",
+    },
+    {
+      name: "Trenches",
+      artist: "Philanthrope, Sleepy Fish",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A15D50", "#995A48"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10247",
+    },
+    {
+      name: "Serendipity",
+      artist: "Philanthrope, The Field Tapes",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A15D50", "#995A48"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10249",
+    },
+    {
+      name: "Pine and Oak",
+      artist: "Philanthrope, The Field Tapes",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A15D50", "#995A48"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10251",
+    },
+    {
+      name: "Droplets",
+      artist: "Philanthrope",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A15D50", "#995A48"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10257",
+    },
+    {
+      name: "Plants",
+      artist: "Philanthrope, Ian Ewing, Sleepy Fish",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A15D50", "#995A48"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10255",
+    },
+    {
+      name: "Soil",
+      artist: "Philanthrope, Guillaume Muschalle",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A15D50", "#995A48"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10254",
+    },
+    {
+      name: "Willow",
+      artist: "Philanthrope, Brock Berrigan, The Field Tapes",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A15D50", "#995A48"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10262",
+    },
+    {
+      name: "Anemone",
+      artist: "Philanthrope, Dotlights",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A15D50", "#995A48"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10259",
+    },
+    {
+      name: "Habitat",
+      artist: "Philanthrope, Kendall Miles",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      id: uuidv4(),
+      color: ["#A15D50", "#995A48"],
+      active: false,
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10265",
+    },
+  ];
+}
+
+export default data;
