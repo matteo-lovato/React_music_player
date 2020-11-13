f4fd6dab9605653a15fb34253b20ed79709de066
matteo_lovato <lovato.teo@gmail.com>
2020-11-13 13:02:19 +0100

    change song from the library

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,13 +1,17 @@
 import React from "react";
 import LibrarySong from "./LibrarySong";
 
-function Library({ songs }) {
+function Library({ songs, setCurrentSong }) {
   return (
     <div className="library">
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map(song => (
-          <LibrarySong song={song} />
+          <LibrarySong
+            song={song}
+            setCurrentSong={setCurrentSong}
+            key={song.id}
+          />
         ))}
       </div>
     </div>
--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,8 +1,11 @@
 import React from "react";
 
-function LibrarySong({ song }) {
+function LibrarySong({ song, setCurrentSong }) {
+  const songSelectHandler = () => {
+    setCurrentSong(song);
+  };
   return (
-    <div className="library-song">
+    <div onClick={songSelectHandler} className="library-song">
       <img src={song.cover} alt={song.name}></img>
       <div className="library-song-description">
         <h3>{song.name}</h3>
